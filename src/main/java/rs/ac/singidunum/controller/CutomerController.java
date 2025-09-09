bda0bb86adcc31d2b4f8c4682cbecc6356617db2
Ana Stancic <anet.persa@gmail.com>
2025-09-09 14:12:20 +0200

    Customer Controller and Repo added

---
+++ src/main/java/rs/ac/singidunum/controller/CutomerController.java
@@ -0,0 +1,28 @@
+package rs.ac.singidunum.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.ac.singidunum.entity.Customer;
+import rs.ac.singidunum.repo.CustomerRepository;
+
+import java.util.List;
+
+//Controller je klasa koja omogucava mapiranje nekih putanja u web pretrazivacu sa nekim kodom gde vracamo podatke u json formatu
+@RestController
+//Ova anotacija nam treba da kazemo koja ce biti osnovna putanja iza svih nasih drugih (sve sto se posalje preko ovog dole rutira se na ovu klasu)
+@RequestMapping(path = "/api/customer")
+//da se dozvoli sa svih origina da poziva api
+@CrossOrigin
+@RequiredArgsConstructor
+public class CutomerController {
+
+    private final CustomerRepository repository;
+
+    @GetMapping
+    public List<Customer> getCustomer() {
+        return repository.findAll();
+    }
+}
---
+++ src/main/java/rs/ac/singidunum/repo/CustomerRepository.java
@@ -0,0 +1,14 @@
+package rs.ac.singidunum.repo;
+
+//repo-ovi se prave kao interfejs jer to koristi dependency injection u pozadini i da on na osnovu interfejsa moze da
+//konstruise (pomocu refleksije u Javi) instancu takvog interfejsa koja bi implementirala pravilno imena tih metoda.
+//To olaksava kako pisemo upite ka bazi
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.singidunum.entity.Customer;
+
+//Ima dva interfejsa koje moze extendovati - CRUD i JPA. Ovaj prvi je stariji, vise basic.
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
